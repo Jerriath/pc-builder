451963c07632b6e766d7a982f4faa9551ee56371
Jerry Zhang <jerryzhang101@yahoo.com>
2022-02-14 13:44:39 -0500

    Category_detail route now works:

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,6 +4,7 @@ const Category = require("../models/category");
 const Manufacturer = require("../models/manufacturer");
 
 // Importing necessary modules
+const mongoose = require("mongoose");
 const async = require("async");
 const { body, validationResults } = require("express-validator");
 
@@ -11,8 +12,38 @@ exports.category_list = function(req, res) {
     res.send("NOT IMPLEMENTED YET");
 }
 
-exports.category_detail = function(req, res) {
-    res.send("NOT IMPLEMENTED YET");
+exports.category_detail = function(req, res, next) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        let err = new Error("Invalid ObjectID");
+        err.status = 404;
+        return next(err);
+    }
+    async.parallel(
+        {
+            category: function (callback) {
+                Category.findById(req.params.id).exec(callback);
+            },
+            components: function (callback) {
+                Component.find({ category: req.params.id})
+                    .populate("category")
+                    .populate("manufacturer")
+                    .exec(callback);
+            }
+        },
+        function (err, results) {
+            if (err) { return next(err) }
+            if (results.category == null) {
+                let err = new Error("Category not found");
+                err.status = 404;
+                return next(err);
+            }
+            res.render("category_detail", {
+                title: results.category.title,
+                category: results.category,
+                components: results.components
+            })
+        }
+    )
 }
 
 exports.category_create_get = function(req, res) {
