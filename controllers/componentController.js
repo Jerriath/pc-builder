70bb17ef45598b92c6d032ff44c81c9d88b87674
Jerry Zhang <jerryzhang101@yahoo.com>
2022-02-14 13:30:37 -0500

    Catalog route now renders list of categories

--- controllers/componentController.js
+++ controllers/componentController.js
@@ -7,8 +7,46 @@ const Manufacturer = require("../models/manufacturer");
 const async = require("async");
 const { body, validationResults } = require("express-validator");
 
-exports.index = function(req, res) {
-    res.render("index", { title: "Components Catalog" })
+function getStoredParts(req, next) {
+    let promises = [];
+    for (const categoryID in req.cookies) {
+      if (
+        mongoose.Types.ObjectId.isValid(categoryID) &&
+        mongoose.Types.ObjectId.isValid(req.cookies[categoryID])
+      ) {
+        promises.push(
+          new Promise(function (resolve, reject) {
+            ComputerPart.findById(req.cookies[categoryID]).exec(function (
+              err,
+              part
+            ) {
+              if (err) return next(err);
+              resolve([categoryID, part]);
+            });
+          })
+        );
+      }
+    }
+    return promises;
+  }
+
+exports.index = function(req, res, next) {
+
+    Category.find({}).exec(function (err, categories) {
+        if (err) return next(err);
+        const userList = {};
+        Promise.all(getStoredParts(req, next)).then(function (parts) {
+            parts.forEach((part) => {
+            userList[part[0]] = part[1];
+            });
+        });
+        res.render("index", {
+            title: "Components Catalog",
+            userList: userList,
+            categories: categories,
+        });
+    })
+
 }
 
 exports.component_list = function(req, res) {
