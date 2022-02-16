987757fe861f1dc53a408381a500e5b63b408285
Jerry Zhang <jerryzhang101@yahoo.com>
2022-02-16 12:12:02 -0500

    Added manufacturers route and list

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -5,14 +5,51 @@ const Manufacturer = require("../models/manufacturer");
 
 // Importing necessary modules
 const async = require("async");
+const mongoose = require("mongoose");
 const { body, validationResults } = require("express-validator");
 
 exports.manufacturer_list = function(req, res) {
-    res.send("NOT IMPLEMENTED YET");
+
+    Manufacturer.find({}).exec(async function (err, manufacturers) {
+        if (err) return next(err);
+        res.render("manufacturer_list", {
+            title: "Manufacturer List",
+            manufacturers: manufacturers
+        });
+    })
+
 }
 
 exports.manufacturer_detail = function(req, res) {
-    res.send("NOT IMPLEMENTED YET");
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        let err = new Error("Invalid ObjectID");
+        err.status = 404;
+        return next(err);
+    }
+    async.parallel(
+        {
+            manufacturer: function (callback) {
+                Manufacturer.findById(req.params.id).exec(callback);
+            },
+            components: function (callback) {
+                Component.find({ manurfacturer: req.params.id})
+                    .populate("category")
+                    .exec(callback);
+            }
+        },
+        function (err, results) {
+            if (err) { return next(err) }
+            if (results.manufacturer == null) {
+                let err = new Error("Manufacturer not found");
+                err.status = 404;
+                return next(err);
+            }
+            res.render("manufacturer_detail", {
+                manufacturer: results.manufacturer,
+                components: results.components
+            })
+        }
+    )
 }
 
 exports.manufacturer_create_get = function(req, res) {
