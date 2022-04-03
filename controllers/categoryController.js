167e028af93dcb203dbf55c2b13a688a72374fbb
Jerry Zhang <jerryzhang101@yahoo.com>
2022-04-03 13:06:11 -0400

    Finished category update routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -159,6 +159,146 @@ exports.category_update_get = function(req, res) {
     }
 }
 
-exports.category_update_post = function(req, res) {
-    res.send("NOT IMPLEMENTED YET");
+exports.category_update_post = [
+
+    body("name")
+        .trim()
+        .isLength({min: 1})
+        .escape()
+        .withMessage("Must provide a manufacturer name"),
+    body("description").optional({checkfalsy: true}),
+    (req, res, next) => {
+       const errors = validationResult(req);
+       if (!errors.isEmpty()) {
+           res.render("manufacturer_form", {
+               title: "Add a manufacturer",
+               manufacturer: req.body,
+               isUpdating: false,
+               errors: errors.array()
+           })
+           return;
+       } 
+       else {
+           let manufacturer = new Manufacturer({
+               name: req.body.name,
+               description: req.body.description
+           });
+           manufacturer.save(function (err) {
+               if (err) {return next(err)}
+               res.redirect(manufacturer.url);
+           })
+       }
+    }
+]
+
+exports.manufacturer_delete_get = function(req, res) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        let err = new Error("Invalid ObjectID");
+        err.status = 404;
+        return next(err);
+    }
+    async.parallel(
+        {
+            manufacturer: function(callback) {
+                Manufacturer.findById(req.params.id).exec(callback)
+            },
+            components: function(callback) {
+                Component.find({manufacturer: req.params.id}).exec(callback)
+            }
+        },
+        function (err, results) {
+            if (err) { return next(err) }
+            if (results.manufacturer == null) {
+                let error = new Error("Manufacturer not found");
+                error.status = 404;
+                return (next(error));
+            }
+            let msg = "";
+            if (results.components.length > 0) {
+                msg = "You cannot delete this manufacturer unless you delete these components first. Click on the component to go to it's page."                
+            }
+            else {
+                msg = "Are you sure you want to delete this manufacturer? "
+            }
+            res.render("manufacturer_delete", {
+                manufacturer: results.manufacturer,
+                components: results.components,
+                msg: msg
+            })
+        }
+    )
 }
+
+exports.manufacturer_delete_post = function(req, res, next) {
+    Manufacturer.findByIdAndDelete(req.params.id)
+        .exec(function(err, next) {
+            if (err) { return next(err) }
+            res.redirect("/");
+        }
+    );
+}
+
+exports.manufacturer_update_get = function(req, res, next) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        let err = new Error("Invalid ObjectID");
+        err.status = 404;
+        return next(err);
+    }
+    else {
+        Manufacturer.findById(req.params.id)
+            .exec(function(err, manufacturer) {
+                if (err) { return next(err) }
+                if (manufacturer == null) {
+                    const err = new Error("Manufacturer not found");
+                    err.status = 404;
+                    return next(err);
+                }
+                res.render("manufacturer_form", { 
+                    title: "Update this manufacturer", 
+                    isUpdating: true, 
+                    manufacturer: manufacturer 
+                })
+            })
+    }
+}
+
+exports.manufacturer_update_post = [
+    body("name")
+        .trim()
+        .isLength({min: 1})
+        .escape()
+        .withMessage("Must provide a cagtegory name"),
+    body("description").optional({checkfalsy: true}),
+    (req, res, next) => {
+       const errors = validationResult(req);
+       if (!errors.isEmpty()) {
+           res.render("category_form", {
+               title: "Add a category",
+               category: req.body,
+               isUpdating: false,
+               errors: errors.array()
+           })
+           return;
+       } 
+       else {
+            Category.findById(req.params.id)
+                .exec(function (error, category) {
+                    if (error) {
+                        return next(error);
+                    }
+                    category.name = req.body.name;
+                    category.description = req.body.description;
+                    category.save(function (error) {
+                        if (error) {
+                            console.log("error");
+                            return next(error);
+                        }
+                        else {
+                            console.log("success");
+                            res.redirect(category.url);
+                        }
+                    })
+                })
+       }
+    }
+]
